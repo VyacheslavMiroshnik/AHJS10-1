27742ec583afd4bc534a2d169cc767f989c7bf45
Vyacheslav Miroshnik <mepa087@gmail.com>
2023-04-13 23:29:50 +0300

    init commit

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,12 @@
+import read from './reader';
+import json from './parser';
+
+export default class GameSavingLoader {
+  static load() {
+    return new Promise((resolve) => {
+      read()
+        .then((buffer) => json(buffer))
+        .then((string) => resolve(JSON.parse(string)));
+    });
+  }
+}
